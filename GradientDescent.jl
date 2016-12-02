977b12869ad49989f7be4de951ca4c6f9f41fb55
zagazao <lukas.heppe@udo.edu>
2016-12-02 23:08:52 +0100

    Ready?

--- GradientDescent.jl
+++ GradientDescent.jl
@@ -5,7 +5,7 @@ function logLikelihood(X,y,theta,lambda)
 	# compute likelihood for each data-point
 	for i in 1:size(X,1)
 		x = X[i:i,1:size(X,2)]
-		funcVal += log(1+ exp(-y[i]*theta'*x')) + lambda/2*norm(theta)^2
+		funcVal += log(1+ exp(-y[i]*theta'*x')) + lambda/2*norm(theta,2)^2
 	end
 	return funcVal[1]
 end
@@ -27,7 +27,7 @@ function logregGD(X, y, lambda, epsilon=1e-4, maxiter=1000,maxLSiter=25, verbose
 	# x in r^n*m (input vector) m*n = m zeilen bei n spalten
 	m = size(X,1) # i have m datapoints
 	n = size(X,2) # n = dimension of the problem	
-	theta = ones(n)
+	theta = rand(n)
 	multiplier = 0.5
 	
 	optimal = false	
@@ -46,7 +46,7 @@ function logregGD(X, y, lambda, epsilon=1e-4, maxiter=1000,maxLSiter=25, verbose
 		# find step size
 		lineSearchIter = 0
 		for j in 1:maxLSiter
-			f_xk_alpha_k = logLikelihood(X,y,theta-(alpha*-gradient),lambda)
+			f_xk_alpha_k = logLikelihood(X,y,theta+(alpha*-gradient),lambda)
 			f_xk_plus_grad = likelihood + (epsilon*alpha*gradient' * -gradient)[1]
 			#println(string(f_xk_alpha_k) * "<" * string(f_xk_plus_grad))
 		
@@ -55,7 +55,7 @@ function logregGD(X, y, lambda, epsilon=1e-4, maxiter=1000,maxLSiter=25, verbose
 				alpha = alpha * multiplier		
 				lineSearchIter += 1
 			else 
-				println("BREAK !!!!!!!!!")
+				#println("BREAK !!!!!!!!!")
 			end	
 		end
 		# verbose
@@ -73,6 +73,33 @@ function logregGD(X, y, lambda, epsilon=1e-4, maxiter=1000,maxLSiter=25, verbose
 	return (theta,"maxIter")
 end
 
+function predict(theta,x)
+	return 1 / (1+exp(-theta'*x')[1])
+end
+
+function evaluate(X,y,theta)
+	positiveHits = 0
+	for i in 1:size(X,1)
+		prediction = predict(theta,X[i:i,1:size(X,2)])
+		label = 1
+		if prediction < 0.5 
+			label = -1
+		end
+		
+		if label == y[i]
+			positiveHits += 1
+		end
+		if label == 1 
+			label = "+1"
+		end
+		labelString = "-1.0"
+		if y[i] == 1.0
+			labelString = "+1.0"
+		end
+		@printf("Prediction : %s | Label : %s | Accuracy %f \n",label,labelString,positiveHits / size(X,1))
+	end
+	return positiveHits / size(X,1)
+end
 dataLocation = "data/mnist67.scale.1k.mat"
 
 # 784 dimensions
@@ -83,4 +110,6 @@ tmp = read(file, "X")
 X = full(tmp)
 y = read(file,"y")
 
-println(logregGD(X,y,0.1,1e-4,1000,25,true))
+theta, state = logregGD(X,y,0.1,1e-4,1000,25,true)
+
+println(evaluate(X,y,theta))
